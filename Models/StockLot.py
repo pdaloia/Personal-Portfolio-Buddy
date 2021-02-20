3b075fa389d94ba8ec284a2bd5295b8b5fc16ffc
Philip D'Aloia <philip.daloia@gmail.com>
2021-02-20 00:43:50 -0500

    Adding model for a stock lot

---
+++ Models/StockLot.py
@@ -0,0 +1,6 @@
+class StockLot:
+
+    def __init__(self, price_per_stock, quantity, date_lot_purchased):
+        self.price_per_stock = price_per_stock
+        self.quantity = quantity
+        self.date_lot_purchased = date_lot_purchased
