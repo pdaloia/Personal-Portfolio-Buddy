a4d166ccbf25079da91b2bc7df880c50157cd5ae
Philip D'Aloia <philip.daloia@gmail.com>
2021-02-20 18:37:58 -0500

    Adding ticker to stock lot model

--- Models/StockLot.py
+++ Models/StockLot.py
@@ -8,7 +8,8 @@ class LotAction(Enum):
 
 class StockLot:
 
-    def __init__(self, price_per_stock, quantity, date_of_action, action_type):
+    def __init__(self, ticker, price_per_stock, quantity, date_of_action, action_type):
+        self.ticker = ticker
         self.price_per_stock = price_per_stock
         self.quantity = quantity
         self.date_of_action = date_of_action
