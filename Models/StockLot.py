baa35eebc8b9889f795df9a639593a65ea0a069e
Philip D'Aloia <philip.daloia@gmail.com>
2021-02-20 18:07:01 -0500

    Adding an enum type for a lot action (buy or sell right now) as well as adding a member to the StockLot class for this

--- Models/StockLot.py
+++ Models/StockLot.py
@@ -1,6 +1,15 @@
+from enum import Enum
+
+
+class LotAction(Enum):
+    Buy = 1
+    Sell = 2
+
+
 class StockLot:
 
-    def __init__(self, price_per_stock, quantity, date_lot_purchased):
+    def __init__(self, price_per_stock, quantity, date_of_action, action_type):
         self.price_per_stock = price_per_stock
         self.quantity = quantity
-        self.date_lot_purchased = date_lot_purchased
+        self.date_of_action = date_of_action
+        self.action_type = action_type
