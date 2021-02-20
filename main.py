8359efcda3dff7698783d108f346e38a5e76bb42
Philip D'Aloia <philip.daloia@gmail.com>
2021-02-20 18:15:43 -0500

    Creating main function to be more Pythonic

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # This is the main py script for Personal Portfolio Buddy
 
-if __name__ == '__main__':
+def main():
     print("Personal Portfolio Buddy")
     print("I don't do much (yet)")
+
+
+if __name__ == '__main__':
+    main()
