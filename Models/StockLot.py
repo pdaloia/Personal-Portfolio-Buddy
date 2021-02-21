6bc2d5813fabc980fb3852299a65000a517479be
Philip D'Aloia <philip.daloia@gmail.com>
2021-02-20 19:48:25 -0500

    Using the LotAction enum to store in the action_type of the StockLot objects

--- Models/StockLot.py
+++ Models/StockLot.py
@@ -4,13 +4,19 @@ from enum import Enum
 class LotAction(Enum):
     Buy = 1
     Sell = 2
+    Unknown = 3
 
 
 class StockLot:
 
     def __init__(self, date_of_action, action_type, ticker, quantity, price_per_stock):
         self.date_of_action = date_of_action
-        self.action_type = action_type
+        if action_type == "Buy":
+            self.action_type = LotAction.Buy
+        elif action_type == "Sell":
+            self.action_type = LotAction.Sell
+        else:
+            self.action_type = LotAction.Unknown
         self.ticker = ticker
         self.quantity = float(quantity)
         self.price_per_stock = float(price_per_stock)
