5ac8e226feb671115b6c20baa39ab1d3b6ca8cf1
Philip D'Aloia <philip.daloia@gmail.com>
2021-02-24 19:22:26 -0500

    Making portfoliohistorybuilder.py more readable with better comments

--- Helpers/portfoliohistorybuilder.py
+++ Helpers/portfoliohistorybuilder.py
@@ -11,8 +11,6 @@ def build_portfolio_history(start_date, end_date, stock_lots):
 
     start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()
     time_delta = datetime.timedelta(days=1)
-    print(start_date)
-    print(end_date)
 
     portfolio = {}
     previous_days_portfolio = {}
@@ -20,23 +18,17 @@ def build_portfolio_history(start_date, end_date, stock_lots):
     # iterate over all days in the range passed to this function
     while start_date <= end_date:
 
-        # create the list to store the current day's stock lot actions
+        # Build a list of actions taken place on the current day
         current_day_actions = []
-
-        # If there are still stock lot actions in the list, check to see if the first one is on the current day.
-        # If it is, pop it into the list
         while stock_lots and str(start_date) == stock_lots[0].date_of_action:
 
             # if there is a hit, pop the first element and add it to the current day's actions
             current_day_actions.append(stock_lots.pop(0))
 
-        # Create the days portfolio with the key of the date and the value being the dictionary of stock quantities
+        # Build the current days portfolio using the previous days portfolio, then set previous day after we are done
         portfolio[str(start_date)] = create_daily_portfolio(current_day_actions, previous_days_portfolio)
-
-        # Now that we are done creating today's value, make the previous day portfolio set to this day
         previous_days_portfolio = portfolio[str(start_date)]
 
-        # Increment the current date by the time delta
         start_date += time_delta
 
     return portfolio
@@ -49,10 +41,8 @@ def create_daily_portfolio(actions, previous_day_portfolio):
     if not actions:
         return previous_day_portfolio
 
-    # create a new copy (to avoid reference errors) of the previous days portfolio for the current day
     daily_portfolio = previous_day_portfolio.copy()
 
-    # do the modifications here
     for action in actions:
         if action.ticker in daily_portfolio:
             daily_portfolio[action.ticker] += action.quantity
